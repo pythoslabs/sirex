9778bce9549ec0e3a3af16502aed2e932218a7aa
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2020-06-28 22:03:18 +0530

    Add files via upload

---
+++ sirex/__init__.py
@@ -0,0 +1,15 @@
+from . import sirex
+
+__version__ = "0.0.3"
+
+
+
+__title__ = "sirex"
+__summary__ = "Collection of functions to make regex calls on strings and numbers"
+__uri__ = "https://github.com/pythoslabs/sirex"
+__author__ = "Pythos Labs"
+__email__ = "pythoslabs@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 Pythos Labs and all contributors"
+
