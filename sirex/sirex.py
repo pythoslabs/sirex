f7da12e33eaf89a6ba432b49a669ae51414406a8
Pythos Labs <pythoslabs@gmail.com>
2020-07-03 11:03:52 +0530

    Update sirex.py
    Added new functions for file related operations

--- sirex/sirex.py
+++ sirex/sirex.py
@@ -1,5 +1,10 @@
 import re 
 
+# ------------------------------------ 
+# Functions related to number / numerics
+# In future versions, will go to sirex.str 
+# ------------------------------------
+
 def count_numbers(str_,blocks=False):
     '''
     Gives the count of numbers present in the string
@@ -72,17 +77,7 @@ def remove_numbers(str_):
     replaced_ = re.sub(pattern, "" , str_ )
     return replaced_.strip()
 
-def count_occurance(sub,parent,case_sensitive = False):
-    '''
-    Counts the occurance of a sub-string in a parent string
-    Usage : sirex.count_occurance('He', 'Hello there' , True) # Searches for h and H
-    Returns : int
-    '''
-    pattern  = sub
-    if not case_sensitive:
-        pattern = "(?i)"+sub
-    count = len((re.findall(pattern,parent)))            
-    return count 
+
 
 # Checks if string ends with a number 
 
@@ -110,8 +105,83 @@ def beginswith_number(str_):
     else : 
         return False
 
+# ------------------------------------ 
+# 			string/text Related Functions 
+# In future versions, will go to sirex.str 
+# ------------------------------------
 
-# ---------- To do 
+def count_occurance(sub,parent,case_sensitive = False):
+    '''
+    Counts the occurance of a sub-string in a parent string
+    Usage : sirex.count_occurance('He', 'Hello there' , True) # Searches for h and H
+    Returns : int
+    '''
+    pattern  = sub
+    if not case_sensitive:
+        pattern = "(?i)"+sub
+    count = len((re.findall(pattern,parent)))            
+    return count 
+
+
+# ------------------------------------ 
+# 			File Related Functions 
+# In future versions, will go to sirex.file 
+# ------------------------------------
+
+def file_to_str(fpath,singleline=True,convert_lower_case = False) : 
+    '''
+    
+    Reads a file and converts all the text into one string 
+    arg ( singleline)  - convert everything to one single line, strips newline and carriage feeds
+    
+    Usage : sirex.file_to_str('f.txt') # Searches for h and H
+    Returns : str
+
+    '''
+    with open(fpath) as f:
+        if singleline :  
+            all_str =  " ".join(line.strip() for line in f)  
+        else : 
+            all_str =  " ".join(line  for line in f)  
+        if convert_lower_case :
+            all_str = all_str.lower()
+    return all_str
+
+def word_count(fname):
+    '''
+
+    Returns the number of words in the file 
+    Usage : 
+    sirex.word_count('f.txt') # Searches for h and H
+    Returns : int
+    
+    '''
+    all_str = file_to_str(fname)
+    return len(re.findall(pattern,all_str))
+
+def line_count(fname ):
+    '''
+    Returns the number of lines in the file 
+    sirex.line_count('f.txt') # Searches for h and H
+    Returns : int
+    '''
+    all_str = file_to_str(fname,singleline=False)
+    return  len(re.findall('\n',all_str)) + 1 
+
+def occurance_file(str_, fname,case_sensitive = False):
+    all_str = file_to_str(fname,convert_lower_case=not case_sensitive)
+    if not case_sensitive:
+        all_str = all_str.lower()
+        str_ = str_.lower()
+    
+    return len(re.findall(str_,all_str))
+
+
+# ------------------------------------ 
+# Functions to be implmented in later versions
+# 
+# ------------------------------------
+
 
 def biggest_number(file_path) :
 	'''
@@ -129,4 +199,4 @@ def remove_stop_words(str_)	:
 	Removes all the words present in a list of stop words 
 	from the input string
 	'''
-	get_stopwords()
+	#get_stopwords()
