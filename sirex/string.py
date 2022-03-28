a52cf48a62bb73fcd9fd42fd3519ce85d3aca4af
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2022-03-28 09:23:47 +0530

    Update string.py

--- sirex/string.py
+++ sirex/string.py
@@ -1,18 +1,15 @@
 # ------------------------------------ 
 # 			string/text Related Functions 
 # ------------------------------------
-
-
 import re 
-
-
 
 def count_occurance(sub,parent,case_sensitive = False):
     '''
     Counts the occurance of a sub-string in a parent string
-    Usage : sirex.count_occurance('He', 'Hello there' , True) # Searches for h and H
+    Usage : sirex.count_occurance('He', 'Hello there' , True) # Searches for 'he' and 'He'
     Returns : int
     '''
+    
     pattern  = sub
     if not case_sensitive:
         pattern = "(?i)"+sub
